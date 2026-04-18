7302447d306549c6ad40c971e02a2140b5cb0043
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-18 18:41:35 +0200

    serving static pages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3003;
@@ -5,6 +6,7 @@ const appRoutes = require("./routes/app.routes");
 
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", appRoutes);
 
