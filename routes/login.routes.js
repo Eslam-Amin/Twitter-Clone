f2d0fd4df3559973c6476a9edbd8ccf23231b832
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-18 17:10:30 +0200

    Added login get endpoint

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 app.set("view engine", "pug");
 app.set("views", "views");
 
-router.get("/", (req, res, next) => {
+router.get("/login", (req, res, next) => {
   const payload = {
     pageTitle: "Login"
   };
