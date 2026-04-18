5344b8f3efc211b4ec4f0534fda62dd9c77d00ff
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-18 19:53:03 +0200

    created a minified registration post endpoint

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -2,14 +2,29 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-app.set("view engine", "pug");
-app.set("views", "views");
-
-router.get("/", (req, res, next) => {
-  const payload = {
-    pageTitle: "Register"
-  };
-  res.status(200).render("register", payload);
-});
+router
+  .get("/", (req, res, next) => {
+    const payload = {
+      pageTitle: "Register"
+    };
+    res.status(200).render("register", payload);
+  })
+  .post("/", (req, res, next) => {
+    const { firstName, lastName, username, email, password, passwordConf } =
+      req.body;
+    let payload = req.body;
+    if (
+      firstName.trim() ||
+      lastName.trim() ||
+      username.trim() ||
+      email.trim() ||
+      password.trim() ||
+      passwordConf.trim()
+    ) {
+    } else {
+      payload.errorMessage = "Make Sure each field has a value";
+    }
+    res.status(200).render("register", payload);
+  });
 
 module.exports = router;
