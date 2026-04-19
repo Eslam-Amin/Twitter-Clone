5d0fee9cc8b7ab4583e7d23d272eb665b33a310b
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-19 17:05:05 +0200

    added timestamps for the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,37 +1,42 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    profilePicture: {
+      type: String,
+      default: "/images/avatar-profile.png"
+    }
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  profilePicture: {
-    type: String,
-    default: "/images/avatar-profile.png"
+  {
+    timestamps: true
   }
-});
+);
 
 const User = mongoose.model("User", userSchema);
 
