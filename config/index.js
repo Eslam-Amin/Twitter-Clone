53285d5687ac4947cbac944850b662ae4eab569c
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-19 17:39:40 +0200

    added session secret key

--- config/index.js
+++ config/index.js
@@ -11,6 +11,9 @@ const db_url = process.env.DB_URL.replace(
 
 const config = {
   port: process.env.PORT || 3003,
+  session: {
+    secret: process.env.SESSION_SECRET
+  },
   database: {
     url: db_url,
     password: process.env.DB_PASSWORD,
