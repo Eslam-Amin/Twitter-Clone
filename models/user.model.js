95525a3916f3ccdc0b054fd3467ea23c82a351cd
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-19 16:57:52 +0200

    Created User model

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: "/images/avatar-profile.png"
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
