b32d5d7d3573ccd79e700d3c7ce145d440291c24
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-21 15:46:20 +0200

    Created post route

---
+++ routes/apis/post.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const User = require("../../models/user.model");
+const Post = require("../../models/post.model");
+
+router
+  .route("/")
+  .get((req, res, next) => {
+    const payload = {
+      pageTitle: "Post"
+    };
+    res.status(200).render("post", payload);
+  })
+  .post(async (req, res, next) => {
+    const { content } = req.body;
+    if (!content) return res.sendStatus(400);
+    const postData = {
+      content,
+      postedBy: req.session.user
+    };
+    let post = Post.create(postData);
+    post = await User.populate(post, { path: "postedBy" });
+    return res.status(201).send(post);
+  });
+
+module.exports = router;
