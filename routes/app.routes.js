a66298f4cc7fd2f032708e3421959f2a92ff3cff
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-21 15:46:55 +0200

    included posts endpoint

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -7,10 +7,12 @@ const middleware = require("../middleware");
 const loginRoutes = require("./login.routes");
 const logoutRoutes = require("./logout.routes");
 const registerRoutes = require("./register.routes");
+const postRoutes = require("./apis/post.routes");
 
 appRoutes.use("/login", loginRoutes);
 appRoutes.use("/logout", logoutRoutes);
 appRoutes.use("/register", registerRoutes);
+appRoutes.use("/api/posts", postRoutes);
 
 appRoutes.get("/", middleware.requireLogin, (req, res, next) => {
   const user = req.session.user;
