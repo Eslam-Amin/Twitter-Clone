afbefcd76a3573b62ef8397374e0ad0da666ab75
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-21 16:37:49 +0200

    added the template for post

--- public/js/common.js
+++ public/js/common.js
@@ -23,5 +23,31 @@ $("#submitPostButton").click((event) => {
 });
 
 function createPostChild(postData) {
-  return postData.content;
+  const postedBy = postData.postedBy;
+  const displayName = `${postedBy.firstName} ${postedBy.lastName}`;
+  const timestamp = postData.createdAt;
+  console.log("🚀 ~ createPostChild ~ postedBy:", postedBy);
+
+  return `
+  <div class="post">
+    <div class="mainContentContainer">
+      <div class="userImageContainer">
+        <img src="${postedBy.profilePicture}" alt="User's Profile Picture">
+      </div>
+      <div class="postContentContainer">
+        <div class="postHeader">
+          <a href="/profile/${postedBy.username}">${displayName}</a>
+          <span class="username">${postedBy.username}</span>
+          <span class="date">${timestamp}</span>
+      </div>
+        <div class="postBody">
+          <span>${postData.content}</span>
+      </div>
+      <div class="postFooter">
+      </div>
+      </*div>
+    </div>
+
+  </>
+  `;
 }
