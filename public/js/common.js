e56c708661f51a6be7f767f88500a7ff797f5b06
Eslam Amin <eslam.amin.610@gmail.com>
2026-04-21 14:45:35 +0200

    Created starter for creating a post

--- public/js/common.js
+++ public/js/common.js
@@ -5,3 +5,16 @@ $("#postTextarea").keyup((event) => {
   if (value.length > 0) $("#submitPostButton").prop("disabled", false);
   else $("#submitPostButton").prop("disabled", true);
 });
+
+$("#submitPostButton").click((event) => {
+  const button = $(event.target);
+  const textbox = $("#postTextarea");
+  const value = textbox.val();
+  const data = {
+    content: value
+  };
+
+  $.post("/api/posts", data, (postData, status, xhr) => {
+    textbox.val("");
+  });
+});
